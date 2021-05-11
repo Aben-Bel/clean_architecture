0a317acdb58e503905618dc40397f1ab49470e0d
aben-bel <abenezer.sbelay@gmail.com>
2021-05-11 09:16:08 +0300

    Clean Architecture setup

---
+++ v1/src/DatabaseMock.java
@@ -0,0 +1,14 @@
+public class DatabaseMock {
+    public int counter;
+    public DatabaseMock(){
+        this.counter = 0;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
